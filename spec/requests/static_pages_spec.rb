4e36dfa35979ff50f2075e1e270005f49474184e
ankitmurdia <krishna.on.earth@gmail.com>
2014-07-04 11:08:47 +0530

    Finish assignment for sample_app2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe "Static pages", :type => :request do
   #  end
   #end
 
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
   	it "should have the content 'Sample App'" do
   		visit '/static_pages/home'
@@ -17,7 +18,7 @@ RSpec.describe "Static pages", :type => :request do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -29,7 +30,7 @@ RSpec.describe "Static pages", :type => :request do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -41,7 +42,19 @@ RSpec.describe "Static pages", :type => :request do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 
